a9d5e1c6f80763287d2609c6ae84ab0d227def8b
Ryann Deyvs <a.deyvs@escolar.ifrn.edu.br>
2024-11-20 18:46:49 -0300

    Atividade 9 COMPLETA

--- Python/E.D.C/Lista de Exercício - 1/Questão - 13.py
+++ Python/E.D.C/Lista de Exercício - 1/Questão - 13.py
@@ -1,1 +1,14 @@
 #Questão: Escreva um programa que leia um número real e faça o arredondamento para inteiro. Se a parte fracionária for maior do que 0.5 o arredondamento deve ser feito para o o próximo inteiro.
+
+número = float(input("Digite um número real: "))
+print()
+
+inteiro = int(número)
+fracionário = número - inteiro
+
+if fracionário > 0.5:
+    resultado = inteiro + 1
+else:
+    resultado = inteiro
+
+print("O número {} arredondado para inteiro é igual a {}".format(número, resultado))
