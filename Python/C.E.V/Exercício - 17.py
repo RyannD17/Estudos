d6bfc6433e3ced2a17552eb037063ccf0ba37cc1
Ryann Deyvs <a.deyvs@escolar.ifrn.edu.br>
2025-01-02 13:11:04 -0300

    TESTE

--- Python/C.E.V/Exercício - 17.py
+++ Python/C.E.V/Exercício - 17.py
@@ -3,3 +3,5 @@ co = float(input("Digite o comprimento do cateto oposto: "))
 ca = float(input("Digite o comprimento do cateto adjacente: "))
 hi = math.hypot(co, ca)
 print(f"\nA hipotenusa vai medir {hi:.2f}")
+
+#casaa
