a92a4339a578bce9a37dbd937ca0742f2aed96f0
Ryann Deyvs <a.deyvs@escolar.ifrn.edu.br>
2025-01-03 16:55:56 -0300

    Atividade 11 em andamento

---
+++ Python/C.E.V/Exercício - 58.py
@@ -0,0 +1,22 @@
+from random import randint
+computador = randint(0, 10)
+
+print("Sou seu computador... Acabei de pensar em um número entre 0 e 10.")
+print("Será que você consegue adivinhar qual foi?")
+
+acertou = False
+palpites = 0
+
+while not acertou:
+    jogador = int(input("\nQual é o seu palpite? "))
+    palpites += 1
+    if jogador == computador:
+        acertou = True
+    
+    else:
+        if jogador < computador:
+            print("MAIS... Tente mais uma vez.")
+        elif jogador > computador:
+            print("MENOS... Tente mais uma vez.")
+
+print(f"\nAcertou com {palpites} tentativas. Parabéns!")
